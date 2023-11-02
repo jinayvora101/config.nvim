8d9bb6ff7e7849ba8d250d5a14c042c39425250d
poggerez <jinayvora101@gmail.com>
2023-11-02 22:14:44 +0530

    adding tabline customisation

--- lua/pogger/options.lua
+++ lua/pogger/options.lua
@@ -5,11 +5,14 @@ o.shiftwidth = 4
 o.number = true
 o.relativenumber = true
 o.cursorline = true
-o.showtabline = 0
+o.showtabline = 2
 o.laststatus = 3
 o.scrolloff = 4
 o.wrap = true
 o.ignorecase = true
+o.hlsearch = true
+
+o.tabline = "%t %m"
 
 vim.cmd [[ autocmd vimenter * if !argc() | NvimTreeOpen | endif ]]
 vim.cmd [[ set clipboard+=unnamedplus ]]
