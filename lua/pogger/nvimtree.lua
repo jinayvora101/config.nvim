7356cf3ca24e09000ce9bf13a9556885001be463
poggerez <jinayvora101@gmail.com>
2023-11-03 20:16:25 +0530

    tidying nvim tree config

--- lua/pogger/nvimtree.lua
+++ lua/pogger/nvimtree.lua
@@ -7,10 +7,8 @@ require("nvim-tree").setup({
 	view = {
 		width = 30,
 		number = true,
-		relativenumber = false,
 	},
 	renderer = {
-		-- group_empty = true,
 		icons = {
 			show = { folder = false, file = false, git = true, folder_arrow = false },
 			glyphs = { git = { unstaged = "<mod", staged = "<stg", unmerged = "<umg", renamed = "<rnm", deleted = "<del", untracked = "<new", ignored = "<ign" }, },
