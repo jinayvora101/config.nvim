845ba8e8c4ecf8395a7a08d94a81adc2465fadb2
poggerez <jinayvora101@gmail.com>
2023-11-05 19:01:57 +0530

    adding tabline support

--- lua/pogger/options.lua
+++ lua/pogger/options.lua
@@ -1,22 +1,21 @@
-vim.cmd [[ autocmd vimenter * if !argc() | NvimTreeOpen | endif ]]
-
 o = vim.opt
+
 
 o.tabstop = 4
 o.shiftwidth = 4
 o.number = true
 o.relativenumber = true
 o.cursorline = true
-o.showtabline = 0
+o.showtabline = 1
 o.laststatus = 3
 o.scrolloff = 8
 o.wrap = true
 o.ignorecase = true
 o.hlsearch = false
 o.showmode = false
-o.tabline = "%t %m"
 o.foldmethod = "indent"
-vim.cmd [[ set nofoldenable ]]
 
+vim.cmd [[ set nofoldenable ]]
 vim.cmd [[ set clipboard+=unnamedplus ]]
+vim.cmd [[ autocmd vimenter * if !argc() | NvimTreeOpen | endif ]]
 
