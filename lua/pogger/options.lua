59047448d73e733ad2f71243decd21d607256832
poggerez <jinayvora101@gmail.com>
2023-11-07 19:33:07 +0530

    adding code folding config

--- lua/pogger/options.lua
+++ lua/pogger/options.lua
@@ -13,9 +13,15 @@ o.wrap = true
 o.ignorecase = true
 o.hlsearch = false
 o.showmode = false
-o.foldmethod = "indent"
+o.splitbelow = true
 
-vim.cmd [[ set nofoldenable ]]
+o.foldmethod = "expr"
+o.foldcolumn = "1"
+o.foldlevel = 20
+vim.cmd [[ set foldexpr=nvim_treesitter#foldexpr() ]]
+-- o.foldexpr = require("nvim-treesitter").foldexpr
+
+vim.cmd [[ set foldenable ]]
 vim.cmd [[ set clipboard+=unnamedplus ]]
 vim.cmd [[ autocmd vimenter * if !argc() | NvimTreeOpen | endif ]]
 
